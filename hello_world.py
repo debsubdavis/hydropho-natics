d373e147105ad2b92daf5d8fe0ccb85f9e9a4317
debsubdavis <95187410+debsubdavis@users.noreply.github.com>
2023-12-03 11:58:58 -0800

    cahnges to hello_world to help pass pylint while verifying environment.yml file works

--- hello_world.py
+++ hello_world.py
@@ -16,6 +16,21 @@ def main():
     """
     Main function that prints the message returned by get_message().
     """
+    df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+
+    print("Pandas DataFrame:\n", df)
+
+    array = np.array([1, 2, 3])
+
+    array_sum = np.sum(array)
+    print("Sum of numpy array:", array_sum)
+
+    spark = pyspark.sql.SparkSession.builder.appName("HelloWorldApp").getOrCreate()
+    print("Spark session created.")
+
+    # Stop the Spark session
+    spark.stop()
+
     message = get_message()
     print(message)
 
