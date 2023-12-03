9f7c45ad82e7bdfa5fc7b76b3001d24f1340fbc0
debsubdavis <95187410+debsubdavis@users.noreply.github.com>
2023-12-02 20:40:52 -0800

    adding test file

--- hello_world.py
+++ hello_world.py
@@ -2,11 +2,18 @@
 A simple script to print "Hello World" that adheres to Pylint standards.
 """
 
+def get_message():
+    """
+    Returns the message to be printed.
+    """
+    return "Hello World"
+
 def main():
     """
-    Main function that prints "Hello World".
+    Main function that prints the message returned by get_message().
     """
-    print("Hello World")
+    message = get_message()
+    print(message)
 
 if __name__ == "__main__":
     main()
