8b2d810186292292f329305439cde4e005ca256f
Emily Creeden <ekrolen@gmail.com>
2024-03-06 13:10:48 -0800

    Cleaned up DELETE_LATER directory and Notes directory. Now to clean sn-final

--- vggish/DELETE_LATER/import numpy as np.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-window_length = 10
-hann_window = 0.5 - (0.5 * np.cos(2 * np.pi / window_length * np.arange(window_length)))
-print(hann_window)
-
-df = pd.DataFrame(hann_window)
-print(df)
