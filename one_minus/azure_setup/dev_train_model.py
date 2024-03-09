eedebab7dd5a91426b9e3e43f62f7780e7f459b8
shweta97m <shweta97@uw.edu>
2024-03-09 10:26:20 -0800

    One Minus - CodeBase

--- one_minus/azure_setup/dev_train_model.py
+++ one_minus/azure_setup/dev_train_model.py
@@ -1,20 +1,23 @@
 from ultralytics import YOLO
+
+# Initialize YOLO model
 model = YOLO()
 
-
+# Access the input dataset provided by Azure ML
 dataset = run.input_datasets['input_data']
 
-# The dataset is a mounted path provided by Azure ML
+# Mount the dataset path
 mounted_path = dataset.as_mount()
 
-# Parse options, ensuring to pass the mounted dataset path for data_dir
+# Parse options, using the mounted dataset path for data_dir
 opt = parse_opt()
 opt.data_dir = mounted_path
 
-
-data_dir = "training_dataset/data.yaml"  
+# Define dataset and output directories
+data_dir = "training_dataset/data.yaml"
 output_dir = "/home/azureuser/cloudfiles/code/Users/djsdavis/training_dataset/output"
 
+# Train the YOLO model using specified options
 results = model.train(data=data_dir, 
                       epochs=1,
                       device="cpu",
